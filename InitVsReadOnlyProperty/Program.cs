04ce01f043efeb7922420dd2d5052f52db8681ad
Saeed Molaiy <101869414+SaeedMolaiy@users.noreply.github.com>
2023-01-21 16:32:50 +0330

    Init Only Vs Read Only Property

---
+++ InitVsReadOnlyProperty/Program.cs
@@ -0,0 +1,23 @@
+﻿using InitVsReadOnlyProperty;
+
+var model = new SomeModel("Test Value", "Test Value");
+
+// you can't set init-only value like read-only property 
+
+// model.ReadOnly = "Saeed Molaiy";
+// model.Init = "Saeed Molaiy";
+
+// so where is difference between init-only and read-only property
+// here we go,with init-only properties you can use object initializer.
+var model2 = new SomeModel("Test Value")
+{
+    Init = "Saeed Molaiy"
+};
+
+var model3 = new SomeModel
+{
+    //it is not possible to use readonly properties with object initializer.
+    Init = "Saeed Molaiy"
+};
+
+Console.ReadKey();
---
+++ InitVsReadOnlyProperty/SomeModel.cs
@@ -0,0 +1,17 @@
+﻿namespace InitVsReadOnlyProperty
+{
+    public class SomeModel
+    {
+        public SomeModel(string readOnly = "", string init = "")
+        {
+            ReadOnly = readOnly;
+            Init = init;
+        }
+
+        //read-only property
+        public string ReadOnly { get; }
+
+        //init-only property
+        public string Init { get; init; }
+    }
+}
